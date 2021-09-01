6d2f998a23b23a364069815504bf2ebdb98f027c
Kristopher C. Castillo <kccastillo2@Kristophers-MacBook-Air-2.local>
2021-09-01 18:07:27 -0400

    Started bonus, didn't finish

--- react-explore-hooks/src/components/ProductView/ProductView.js
+++ react-explore-hooks/src/components/ProductView/ProductView.js
@@ -2,10 +2,34 @@ import React from "react";
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import './ProductView.css'
+import { useState, useEffect } from 'react';
 
 function ProductView({ products }) {
     // TODO: Replace with state variable
-    const sideOpen = true;
+    // const sideBarStatus = localStorage.getItem('sideBarStatus')
+    // const booleanStatus Boolean(sideBarStatus);
+    const [sideOpen, setSideOpen] = useState('');
+    const [product, setProduct] = useState('');
+    
+
+    useEffect(() => {
+        console.log(`selectedProduct CHANGED TO`, product);
+        if (product) {
+            setSideOpen(true);
+        }
+    }, [product])
+
+    useEffect(() => {
+        console.log(`sideOpen CHANGED TO`, sideOpen);
+        if (!sideOpen) setProduct('');
+    }, [sideOpen]);
+
+    useEffect(() => {
+        localStorage.setItem('sideBarStatus', sideOpen)
+        const sideBarStatus = localStorage.getItem("sideBarStatus");
+        setSideOpen(Boolean(sideBarStatus))
+    },[sideOpen])
+ 
 
     return (
         <div className="product-view">
@@ -16,19 +40,21 @@ function ProductView({ products }) {
                         <ProductListItem
                             key={item.id}
                             product={item}
-                            onClick={() => console.log('SELECT PRODUCT', item)}
+                            onClick={() => setProduct(item)}
+                            isSelected={product.id === item.id}
                         />
                     )}
                 </div>
             </div>
+
             <div className="product-side-panel">
                 <div className="product-side-panel-toggle-wrapper">
                     <div className="product-side-panel-toggle"
-                         onClick={() => console.log('TOGGLE SIDE PANEL')}>
+                         onClick={() => setSideOpen(!sideOpen)}>
                         {sideOpen ? '>' : '<'}
                     </div>
                 </div>
-                <ProductDetails visible={sideOpen} />
+                <ProductDetails visible={sideOpen} product={product} />
             </div>
         </div>
     );
